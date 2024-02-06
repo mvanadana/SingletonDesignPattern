cc4cff2632e7b10af0758625405886cf9015ecc8
Vandana Mishra <149364066+mvanadana@users.noreply.github.com>
2024-02-06 16:45:25 +0530

    Update Room.cs

--- Singleton/Room.cs
+++ Singleton/Room.cs
@@ -1,28 +1,32 @@
-﻿using System;
+using System;
 namespace Singleton
 {
     public class Room
     {
+        
         private static Room instance;
 
-        // Private constructor to prevent instantiation from outside the class
+        
         private Room()
         {
             Console.WriteLine("Room instance created.");
         }
 
+        
         public static Room getInstance()
         {
-            get
-            {
-                // Create the instance if it doesn't exist
+            
+            
+                
                 if (instance == null)
                 {
                     instance = new Room();
                 }
                 return instance;
-            }
+            
         }
+
+        
         public void Enter()
         {
             Console.WriteLine("Someone entered the room.");
