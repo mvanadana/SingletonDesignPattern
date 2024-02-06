479bbf0cc97f2f16e9cdaba31c196600d7b0dd8f
Vandana Mishra <149364066+mvanadana@users.noreply.github.com>
2024-02-06 16:40:04 +0530

    Update Program.cs

--- Singleton/Program.cs
+++ Singleton/Program.cs
@@ -6,13 +6,9 @@ namespace Singleton
     {
         static void Main(string[] args)
         {
-            // Get the room instance
+
             Room myRoom = Room.Instance;
-
-            // Call a method on the room instance
             myRoom.Enter();
-
-            // Try to create another instance (will get the existing one)
             Room anotherRoom = Room.Instance;
 
             // Check if both references point to the same instance
